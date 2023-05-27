c64b8df9af301288ae99afd44e7d7f66fdbdda07
Ailton de Souza Junior <ailton.souza97@hotmail.com>
2023-05-27 18:04:29 -0300

    Users page finished

--- routes/users.js
+++ routes/users.js
@@ -25,7 +25,9 @@ router.get("/", async(req, res)=>{
       let users = await User.find({"isActive": "True"}).sort({"name": 1});
       let results = await User.countDocuments({"isActive": "True"});
       let isAdmin = req.user.isAdmin;
+      let loggedin = req.user.username
       res.render("users", {
+        loggedin: loggedin,
         users: users,
         results: results,
         isAdmin: isAdmin
@@ -101,22 +103,62 @@ router.get("/inactiveusers", async(req, res)=>{
 
 //Will reset password to 123456
 router.get("/resetpassword/:id", async(req, res)=> {
-  await User.updateMany({ _id: req.params.id },{
-      $set: {
-        salt: process.env.SALT,
-        hash: "327a1694cb0fdd444d70b7e14ef745b6da24886837405b59151696ce64d626a925b14892cb9597bd4da83caeffbb8f7a73f48bcea2b4aa9538d1161f707fa97d0d514f2f3360fe264c496e6a7b73d9b441db2cfc79a3028ba99b17c3639f42e71ec7b6b76df219240d817e9c81b578e6c5acb1c909694f41100ccba2db775ffa4ad573c3c0d8243a388e7b105648748c5b1fbaaa8bbf55c3fbc12f16f3165b053f2d6c6fbfa3b99b0a4f40cd7ed8fed4881810f468904f4602c2fefe32537c144a05c530a58bb558c752194d554973eb8a64f48b579aac593ff7b36e30bb3854881617395772ae662227aa0a7f09b506304032ee7f1495981ce7c3101ab9aad7b2624ce441ff40e16e3b98eb4225b310b0c6b065b4281e130e9d935d98e81754718cf24881017bc31d90da7bc302b14936b5fa778b0ee28ff9336364c4ecff163e9371459d1fe1a8c1b64890d1cf1093b16544f068d46462f805282b77dce347f6bc59be2a8e9cf6cce66106f6edaaa8d177b72bf35d9cebb4c5cbbd989d0908b1743157f059d61cca8a334b10eec4b824fcf83d783cc3655a865d092f5724fd05ad31497d5c00096230097a1e4d6286f1ba661592cdde03b6ec47dbcf3989f9df9ea64a542a36bc7bfdf49d001e274ec94690d50478d5b180ea013324eac01c0cadd7df594689d0fc2e67ebef1811c8789b6caa4b6acb9dce50d4f4835deefe"
-      }
-    })
+  if(req.isAuthenticated()){
+    try {
+      await User.updateMany({ _id: req.params.id },{
+        $set: {
+          salt: process.env.SALT,
+          hash: "35718566d516cba90efc30b3c99a79deb43cbda7f4827976e975296e351f5f0efa416d7a23a01db2162e8ea4f51cf8f20c3071d632b94db29b1b41b19e8300175621424851ef77a84960c1661ce85a82ce40044185777ed2f0d0085a7ce2e5526d3d687d8983e859714fc1844de00171bb02a927578c71c8b077343be60401f9aeb7aad095886a6e820b34aeaadc1a55689e1f2cea51bf1ad32ce35ecb00b43c1ebaeef56d87facf22a1b16ad08c9d2792333ef9b0affb5bc1b06ca88715f527ae403a6cd6f35bce77972b6d7c71ce2e6488ccfd4a07ab8946cb3932fbfc71131f10d7c97dc1020f93c1ac3205335cb02817642f8bc367ed84899135132d93e4db9b3b95b52b1182196f042c3b3c17ac5070137153136bcf875a2f424f9ec3cf1422586718c0dc83cde66b53edfd06bb1d22acc5cb3daeee579b57858c1f43461edb57547fbce3d5d05b6f1dd5811339d2fa6ee0cbe900f58970d20dcfb72a607ac6335fe6fe9dbe33c99117b91a92a5d559823141064018a6e72936dc069c0056be1e05a9c735d35ea884a3b35a126743eeeb9b4668f179e4a99a1e1a9d2a65ecba8a39cfef18f60581730904c6c94835ee257874bb712aec3911efff35e08434c02a92575a15f67c6d40743769cdeb78583b91f0fd6db3b997db463618dd5f7e79ceab90a2c6d4bcf27e652fe806a7eb8a5359843cb24be73b83cca9e5f01f"
+        }
+      })
+      res.redirect("/users/")
+    } catch (error) {
+      res.render("error.ejs")
+    }
+  }
 });
 
-router.use("/trocasenha", function (req, res) {
+router.get("/edituser/:id", async(req, res) =>{
   if (req.isAuthenticated()) {
-      Chamado.find(function (err, chamado) {
-        res.render("trocasenha", {
-          user: req.user._id,
-          chamado: chamado,
-        });
-      });
+    try {
+      let user = await User.findOne({"_id": req.params.id})
+      res.render("usersedit",{
+        user: user
+      })
+    } catch (error) {
+      res.render("error.ejs")
+    }
+  } else {
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+});
+
+router.post("/edituser/:id", async(req, res) =>{
+  if (req.isAuthenticated()) {
+    try {
+      let updates = req.body
+      await User.findOneAndUpdate({"_id": req.params.id}, updates)
+      res.redirect("/users/")
+    } catch (error) {
+      res.render("error.ejs")
+    }
+  } else {
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+  }
+});
+
+router.get("/changepassword/:id", async(req, res) =>{
+  if (req.isAuthenticated()) {
+    try {
+      let loggedin = req.user
+      res.render("userschangepassword",{
+        loggedin: loggedin
+      })
+    } catch (error) {
+      res.render("error.ejs")
+    }
   } else {
     req.session.returnTo = req.originalUrl;
     res.redirect("/login");
@@ -126,29 +168,17 @@ router.use("/trocasenha", function (req, res) {
 router.post('/changepassword/:id', function (req, res) {
   User.findById(req.params.id, (err, user) => {
       if (err) {
-          res.send(err);
+          res.render("error.ejs");
       } else {
           user.changePassword(req.body.oldpassword, 
           req.body.newpassword, function (err) {
               if (err) {
-                  res.send("Não foi possível trocar senha")
+                  res.render("error.ejs")
               } else {
-                  res.redirect('/inicio')
+                  res.redirect('/users/')
               }
           });
       }
-  });
-});
-
-//Delete usuários da página dados.
-router.post("/deleteUser", function (req, res) {
-  User.deleteOne({ username: req.body.username }, function (err) {
-    if (err) {
-      console.log(err);
-      res.send("Erro ao excluir");
-    } else {
-      res.redirect("/ativos");
-    }
   });
 });
 
