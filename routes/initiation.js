67d18ed90b53949257fb87eb32b1ecad791954f0
Ailton <ailton.souza97@hotmail.com>
2023-05-31 14:43:36 -0300

    Minor changes

--- routes/initiation.js
+++ routes/initiation.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {login, landing, authenticate, home, logout} = require('../controllers/login.js');
 
-
-
+//Login and authentication
 router.get("/", landing);
 router.get("/login", login);
 router.post("/authenticate", authenticate);
 router.get("/home", home);
 router.get("/logout", logout);
+
+
 
 module.exports = router;
