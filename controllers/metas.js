faf867f28717bca029f35097c82ab0cd8dedaca9
Ailton De Souza Junior <ailton.souza97@hotmail.com>
2023-09-15 00:47:07 -0300

    Adjusted dates

--- controllers/metas.js
+++ controllers/metas.js
@@ -35,24 +35,6 @@ const atualizar = async(req, res)=>{
 const atualizarPost = async(req, res)=>{
     if(req.isAuthenticated() && req.user.isActive == "True"){
         try {
-            function ExcelDateToJSDate(serial) {
-                var utc_days  = Math.floor(serial - 25569);
-                var utc_value = utc_days * 86400;                                        
-                var date_info = new Date(utc_value * 1000);
-             
-                var fractional_day = serial - Math.floor(serial) + 0.0000001;
-             
-                var total_seconds = Math.floor(86400 * fractional_day);
-             
-                var seconds = total_seconds % 60;
-             
-                total_seconds -= seconds;
-             
-                var hours = Math.floor(total_seconds / (60 * 60));
-                var minutes = Math.floor(total_seconds / 60) % 60;
-             
-                return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate());
-             }
             await Metas.deleteMany()
             const file = reader.readFile('storage/metas/' + req.file.filename)
             
@@ -62,18 +44,12 @@ const atualizarPost = async(req, res)=>{
             
             for(let i = 0; i < sheets.length; i++){
             const temp = reader.utils.sheet_to_json(
-                    file.Sheets[file.SheetNames[i]])
+                    file.Sheets[file.SheetNames[i]], {raw: false})
             temp.forEach((res) => {
                 data.push(res)
             })
         }
         await Metas.create(data)
-
-        data.forEach(async (datas) => {
-            console.log(ExcelDateToJSDate(datas.Data))
-        });
-
-
         res.redirect('/metas')
         } catch (error) {
             console.log(error)
