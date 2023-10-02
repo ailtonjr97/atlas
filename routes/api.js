e80a92bede0ba9b8f6adfa73abf4a7a3cbe6d0a9
ailtonjr97 <ailton.souza97@hotmail.com>
2023-10-02 20:20:30 -0300

    Minor changes

--- routes/api.js
+++ routes/api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const{plastMetaConsultar} = require("../controllers/API/api.js")
 
 router.get("/plastmeta/consultar", plastMetaConsultar);
+router.get("/metas", metas)
 
 module.exports = router;
